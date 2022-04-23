0109c94bfdefeb2a7ab11ced1081a7cd5649a97b
Andrew Li <andrewli-1920@hotmail.com>
2022-04-23 18:55:00 +0800

    fixed issues with compilation targeting wrong libs, as well as main definition conflict

--- main.cpp
+++ main.cpp
@@ -1,27 +1,33 @@
 #include <stdio.h>
+#define SDL_MAIN_HANDLED
 #include <SDL.h>
+
+typedef Uint32 u32;
 
 static const int width = 640;
 static const int height = 480;
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
     bool quit = false;
     bool lmb_down = false;
     SDL_Event event;
 
-    SDL_Init(SDL_Init_Video);
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("Failed to initialise SDL\n");
+        return 0;
+    }
     SDL_Window* window     = SDL_CreateWindow("Blank canvas", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_Texture* texture   = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, width, height); // pixles are 32 bit values, 8 bits for each channel
 
-    Uint32* pixels = (UInt32*)malloc(width * height * sizeof(Uint32*));
-    memset(pixels, 255, width * height * sizeof(UInt32)); // clear to white
+    u32* pixels = (u32*)malloc(width * height * sizeof(u32));
+    memset(pixels, 255, width * height * sizeof(u32)); // clear to white
     
     while(!quit) {
-        SDL_UpdateTexture(texture, NULL, pixels, width * sizeof(Uint32));
+        SDL_UpdateTexture(texture, NULL, pixels, width * sizeof(u32));
         SDL_WaitEvent(&event);
         switch(event.type) {
-            case SDL_Quit: {
+            case SDL_QUIT: {
                 quit = true;
                 break;
             }
@@ -31,7 +37,7 @@ int main(int argc, char** argv) {
             }
             case SDL_MOUSEBUTTONDOWN: {
                 if (event.button.button == SDL_BUTTON_LEFT) lmb_down = true;
-                break:
+                break;
             }
             case SDL_MOUSEMOTION: {
                 if (lmb_down) {
@@ -47,7 +53,7 @@ int main(int argc, char** argv) {
         SDL_RenderPresent(renderer);
     }
     SDL_DestroyWindow(window);
-    SDL_Destroy_Renderer(renderer);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyTexture(texture);
     free(pixels);
     SDL_Quit();
