af13277c86962b7a917b1a6e146aabd34b7d56e6
Andrew Li <andrewli-1920@hotmail.com>
2022-05-09 13:28:37 +0800

    required math and data structures added.

--- src/obj_loader.h
+++ src/obj_loader.h
@@ -1,8 +1,12 @@
 #ifndef obj_loader_h
 #define obj_loader_h
 
+#include "renderer_math.h"
+
 namespace Obj {
-    
+    void read_obj_file(const char* path, vec3 out_vertices, vec3 out_uvs, vec3 out_normals) {
+        
+    }
 }
 
 #endif
